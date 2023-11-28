176a9d6cf1f9fe64e24149350ea45d7c39fee994
Sarah O'Reilly <88727212+soreilly6@users.noreply.github.com>
2023-11-28 12:02:04 +0000

    Update getLastPageViewed.js

--- decisionTemplates/getLastPageViewed.js
+++ decisionTemplates/getLastPageViewed.js
@@ -1,1 +1,37 @@
 
+(function () {
+
+  var viewPage;
+  var currentSession = getMostRecentSession();
+  
+  if (currentSession.createdAt){
+    var viewTime;
+    print(viewTime);
+    for (var i = 0; i < currentSession.events.length; i++) {
+        var currentEvent = currentSession.events[i];
+          
+        if(currentEvent.type === "VIEW"){
+            if(currentEvent.createdAt > viewTime || viewTime === undefined){
+                viewTime = currentEvent.createdAt;
+                viewPage = currentEvent.arbitraryData.page;
+            }
+        }
+    }
+    if (viewPage === "/"){
+     viewPage = "homepage" 
+        
+    }
+  }
+
+  return viewPage;
+})();
+
+function getMostRecentSession() {
+    var webSessions = [];
+    guest.sessions.forEach(function (session) {
+        if (session.sessionType === 'WEB') {
+            webSessions.push(session);
+        }
+    });
+    return webSessions[0];
+}
