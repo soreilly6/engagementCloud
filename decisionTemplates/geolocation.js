bd339772f8049be3ba1642b2b3d72318ded644cd
Sarah O'Reilly <88727212+soreilly6@users.noreply.github.com>
2025-04-23 10:33:42 +0100

    Update geolocation.js

--- decisionTemplates/geolocation.js
+++ decisionTemplates/geolocation.js
@@ -1,5 +1,4 @@
 
-
 (function () {
 
   var output;
@@ -8,15 +7,15 @@
       
       var currentSession = getMostRecentSession();
       
-      print("getting current session");
+      
       if (currentSession.createdAt){
         var viewTime;
-        for (var i = 0; i < currentSession.events.length; i++) {
+        for (var i = 0; i < currentSession.dataExtensions.length; i++) {
             
             if(currentSession.dataExtensions){
                 
                 var ext = currentSession.dataExtensions[i].values;
-            
+                
                 if(currentSession.dataExtensions[i].key === "bxt"){
                     output = currentSession.dataExtensions[i].values;
                     output['viewTime'] = new Date(currentSession.createdAt);
